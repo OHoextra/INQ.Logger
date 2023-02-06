9e6f6a42431feb554510887f41331c87713c04eb
Olaf Hoekstra <o.hoekstra@hva.nl>
2023-02-06 18:17:57 +0100

    - Add WebApplicationBuilderExtensions

---
+++ INQ.Logger.Serilog/Extensions/WebApplicationBuilderExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace INQ.Logger.Serilog.Extensions;
+
+public static class WebApplicationBuilderExtensions
+{
+    public static void UseSerilog(this WebApplicationBuilder appBuilder)
+    {
+        var logger = LoggingHelper.InitializeLogger(appBuilder.Configuration);
+
+        appBuilder.Logging.ClearProviders();
+        appBuilder.Logging.AddSerilog(logger);
+        appBuilder.Host.UseSerilog(logger);
+    }
+}
