b3d8b6a2093b7555d8e344c20e1141e4f3c658c5
Olaf Hoekstra <o.hoekstra@hva.nl>
2023-02-07 18:47:01 +0100

    - Refactor

--- INQ.Logger.Serilog/Extensions/LoggerExtensions.cs
+++ INQ.Logger.Serilog/Extensions/LoggerExtensions.cs
@@ -5,27 +5,13 @@ namespace Sandbox.Application.Extensions;
 
 public static class LoggerExtensions
 {
-    private const LogEventLevel DEFAULT_LEVEL = LogEventLevel.Information;
-    private const string DEFAULT_PREFIX = "*** ";
-
-    public static void LogPrefixed(
-        this ILogger logger,
-        string messageTemplate,
-        string prefix = DEFAULT_PREFIX,
-        LogEventLevel level = DEFAULT_LEVEL,
-        params object?[]? propertyValues) 
-        => logger.Log(messageTemplate, prefix, level, propertyValues);
-
     public static void Log(
         this ILogger logger,
-        string messageTemplate,
-        string prefix = "",
-        LogEventLevel level = DEFAULT_LEVEL,
+        string messageTemplate = "",
+        LogEventLevel level = LogEventLevel.Information,
         params object?[]? propertyValues)
     {
-        if (!string.IsNullOrWhiteSpace(prefix))
-            messageTemplate = prefix + messageTemplate;
-
-        logger.Write(level, messageTemplate, propertyValues);
+        if (!string.IsNullOrWhiteSpace(messageTemplate))
+            logger.Write(level, messageTemplate, propertyValues);
     }
 }
